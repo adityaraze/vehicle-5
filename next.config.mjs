4a7cfadc46ad272cf2d759b831597bc5e7d70824
adityaraze <adityamodanwal293@gmail.com>
2025-09-12 10:34:31 +0530

    changes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,52 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental:{
+//     serverComponentsHmrCache:false, // default to true
+//   },
+
+//   images: {
+//     remotePatterns:[
+//     {
+//       protocol:"https",
+//       hostname:"sqgbwjytxzqgkckbelyt.supabase.co",
+//     }
+//     ]
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: "/embed",
+//         headers: [
+//           {
+//             key: "Content-Security-Policy",
+//             value: "frame-src 'self' ",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental:{
-    serverComponentsHmrCache:false, // default to true
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 add this
   },
 
   images: {
-    remotePatterns:[
-    {
-      protocol:"https",
-      hostname:"mkjdlgydzuzhbfhuqwzh.supabase.co",
-    }
-    ]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sqgbwjytxzqgkckbelyt.supabase.co",
+      },
+    ],
   },
+
   async headers() {
     return [
       {
